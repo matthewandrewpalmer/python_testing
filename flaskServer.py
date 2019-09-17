62ed79ae83bb6e022c5d525643f481a0aa76ecc7
Matthew Palmer <matthewandrewpalmer@outlook.com>
2019-09-17 14:49:18 +0100

    Login and Homepage

--- flaskServer.py
+++ flaskServer.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 import redis
 from flask import Flask, render_template, request, escape, make_response
@@ -5,22 +6,20 @@ from flask import Flask, render_template, request, escape, make_response
 app = Flask(__name__)
 
 
+# Simple Hello world
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route("/")
-def get_home():
-    return render_template("index.html", msg='')
-
-
+# Gets name argument from url like ?name=Bob
 @app.route('/world')
 def get_name():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
 
+# Cookie Example: Connecting to redis
 @app.route("/user")
 def get_user_session():
     user_id = request.cookies.get('user_id')
@@ -47,6 +46,32 @@ def get_user_session():
     return response
 
 
+# =========================================   Login and Authentication Website ==================================
+
+@app.route("/")
+def home():
+    # Get cookie from request
+    user_session = request.cookies.get('user_session')
+    r = redis.Redis(host='localhost', port=6379, db=1)
+
+    if user_session is not None:  # We have received a cookie
+        try:
+            user = bytes.decode(r.get(user_session))
+            print(f'Cookie Received from user: {user}')
+            message = f'Hello {user}'
+            return render_template("home.html", msg=message, user=user, logged_in=True)
+        except TypeError:
+            print('Cookie received is invalid')
+            return render_template("home.html", msg='Login session could not be validated, please log in again',
+                                   user='Guest', logged_in=True)
+
+    elif user_session is None:
+        print("No Cookie Received")
+        message = 'Hello Guest'
+        user = 'Guest'
+        return render_template("home.html", msg=message, user=user, logged_in=False)
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -57,46 +82,77 @@ def login():
         print(f'Received password {password}')
 
         # Connect to redis and get password for redis
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        db_password = r.get(user_name)
+        r = redis.Redis(host='localhost', port=6379, db=1)
+        user_data = r.get(user_name)
 
-        if db_password is None:
-            # user not in redis
+        if user_data is None:
+            # If user session not in Redis
             return render_template('login.html', msg=f'Tried to login with user: {user_name} but no user was found.')
-        elif db_password is not None:
+        elif user_data is not None:
             # If user found in redis
-            if password == bytes.decode(db_password):
-                # if submitted password match redis password
-                return render_template('login.html', msg=f'You are now logged in: {user_name}')
+            user = json.loads(bytes.decode(user_data))
+            if password == user['password']:
+                # if submitted password match Redis password
+
+                # Load Homepage
+                response = make_response(
+                    render_template('home.html', msg=f'You are now logged in: {user_name}', user=user_name,
+                                    logged_in=True), 200)
+
+                # Create new uuid to track session
+                user_session = str(uuid.uuid4())
+                print(f'Set a new cookie: {user_session}')
+                # Create Cookie which expires in 1 minute (max_age in seconds)
+                response.set_cookie(key='user_session', value=user_session, max_age=60)
+
+                # Store the uuid with username in redis
+                r.set(str(user_session), user_name)
+
+                return response
             else:
                 # Password Incorrect
                 return render_template('login.html', msg=f'Password Incorrect for user: {user_name}')
     elif request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html', msg='Please Login')
 
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/register', methods=['GET'])
+def register_form():
+    return render_template('register.html', msg='Please Signup')
+
+
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        # Get Username and password from Form
-        user_name = request.form['username']
-        password = request.form['password']
-        print(f'Received username {user_name}')
+    # Get Username and password from Form
+    user_name = request.form['username']
+    password = request.form['password']
+    print(f'Received username {user_name}')
 
-        # Connect to redis and store username and password
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        db_user = r.get(user_name)
-        if db_user is None:
-            print('New User, creating account')
-            r.set(str(user_name), str(password))
+    # Connect to redis and store username and password
+    r = redis.Redis(host='localhost', port=6379, db=1)
+    db_user = r.get(user_name)
+    if db_user is None:
+        print('New User, creating account')
 
-            # redirect to /login page
-            return render_template('login.html', msg=f'Account created with username: {user_name}, please login')
-        else:
-            print('User already exist')
-            return render_template('register.html', msg='Username already in use')
-    elif request.method == 'GET':
-        return render_template('register.html', msg='Please Signup')
+        r.set(str(user_name), json.dumps({'username': user_name, 'password': password}))
+
+        # redirect to /login page
+        return render_template('login.html', msg=f'Account created with username: {user_name}, please login')
+    else:
+        print('User already exist')
+        return render_template('register.html', msg='Username already in use')
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    user_session = request.cookies.get('user_session')
+    r = redis.Redis(host='localhost', port=6379, db=1)
+
+    r.delete(user_session)
+
+    message = 'You have Now been logged out'
+    user = 'guest'
+    return render_template("home.html", msg=message, user=user, logged_in=False)
 
 
 app.run()
